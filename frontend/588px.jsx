3945f03fd69520ed0cd036b71526506296fa5917
Brandon <estanob@gmail.com>
2021-02-09 19:10:57 -0800

    Configure webpack and server

--- frontend/588px.jsx
+++ frontend/588px.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from './store/store'
+// import configureStore from './store/store'
 import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  let store;
-  if (window.currentUser) {
-    const preloadedState = {
-      entities: {
-        users: { [window.currentUser.id]: window.currentUser }
-      },
-      session: { id: window.currentUser.id }
-    };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
+  // let store;
+  // if (window.currentUser) {
+  //   const preloadedState = {
+  //     entities: {
+  //       users: { [window.currentUser.id]: window.currentUser }
+  //     },
+  //     session: { id: window.currentUser.id }
+  //   };
+  //   store = configureStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+  //   store = configureStore();
+  // }
   
-  const closeDropdown = (e) => {
-    if(e.target.className !== '#profilepointer') {
-      $('#profiledropdown').removeClass('trigger')
-    }
-  } 
+  // const closeDropdown = (e) => {
+  //   if(e.target.className !== '#profilepointer') {
+  //     $('#profiledropdown').removeClass('trigger')
+  //   }
+  // } 
 
-  document.addEventListener('click', closeDropdown)
+  // document.addEventListener('click', closeDropdown)
   
-  ReactDOM.render(<Root store={store}/>, root);
+  ReactDOM.render(<h1>Hello!</h1>, root);
 });
