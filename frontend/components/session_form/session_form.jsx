4053baf6d80a469dc82e7a0211beeacedef69f11
Brandon <estanob@gmail.com>
2021-02-10 11:58:02 -0800

    Create frontend actions for User Login

---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.guestUserSubmit = this.guestUserSubmit.bind(this)
+  }
+
+  handleField(field) {
+    return e => {
+      this.setState({ [field]: e.target.value })
+    }
+  }
+
+  guestUserSubmit(e) {
+    e.preventDefault()
+    const guestUser = {
+      username: 'guest',
+      password: '123456'
+    }
+    this.props.processForm(guestUser)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state);
+  }
+
+  componentDidMount() {
+    this.props.clearErrors()
+  }
+
+  signUp() {
+    return (
+      <div>
+        <p>Don't have an account? <Link to='/signup'>Sign Up</Link></p>
+      </div>
+    )
+  }
+
+  login() {
+    return (
+      <div>
+        <p>Already have an account? <Link to='/login'>Log In</Link></p>
+      </div>
+    )
+  }
+
+  usernameError() {
+    return (this.props.errors.map((error, i) => (
+      error.includes('Username') ? <ul className="error" key={i}>{error}</ul> : ''
+    )))
+  }
+
+  passwordError() {
+    return (this.props.errors.map((error, i) => (
+      error.includes('Password') ? <ul className="error" key={i}>{error}</ul> : ''
+    )))
+  }
+
+  render() {
+    const { formType, errors } = this.props
+
+    const LoginLink = (formType === 'signup') ? this.login() : this.signUp()
+
+    const err = errors.map((error, i) => {
+      return <label key={i}> {error}</label>
+    })
+
+    const submitButton = (formType === 'signup') ? 'Sign up' : 'Log in'
+
+    return (
+      <>
+
+
+        <div className="login_form_div">
+          {formType === 'login' && err.length > 0 &&
+            <div onClick={this.props.closeModal} className="modal-errors">
+              {err}
+            </div>
+          }
+
+          <form onSubmit={this.handleSubmit} className="login_form">
+            <h3>
+              {formType === 'login' ? 'Log in to Pinhole' : 'Join Pinhole'}
+            </h3>
+            <br />
+            <br />
+            <label>Email or Username*
+              <br />
+              <input
+                type="text"
+                onChange={this.handleField('username')}
+                value={this.state.username} />
+            </label>
+            <div className='errors-box'>{this.usernameError()}</div>
+            <br /><br />
+            <label>Password*
+              <br />
+              <input
+                type="password"
+                onChange={this.handleField('password')}
+                value={this.state.password} />
+            </label>
+            <div className='errors-box'>{this.passwordError()}</div>
+            <br /><br />
+            <input
+              type="submit"
+              className="login_button"
+              value={submitButton} />
+            <br />
+            {formType === 'login' && <input
+              type="submit"
+              onClick={this.guestUserSubmit}
+              className="login_button"
+              value='Demo User Login' />}
+            <br />
+            <div className='form_question'>
+              {LoginLink}
+            </div>
+          </form>
+        </div>
+      </>
+    )
+  }
+}
+export default SessionForm;
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-// import RootReducer from "../reducers/root_reducer";
+import RootReducer from "../reducers/root_reducer";
 
-// const configureStore = (preloadedState = {}) => {
-//   return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-// }
+const configureStore = (preloadedState = {}) => {
+  return createStore(
+    RootReducer, 
+    preloadedState, 
+    applyMiddleware(thunk, logger)
+  )
+}
 
-// export default configureStore;
+export default configureStore;
