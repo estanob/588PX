30b523204fc359ee974b1d58594eb7055688a2cb
Brandon <estanob@gmail.com>
2021-03-08 11:08:14 -0800

    Fix styling for home feed display when logged in

--- frontend/components/pictures/picture_index_item.jsx
+++ frontend/components/pictures/picture_index_item.jsx
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 
 class PictureIndexItem extends React.Component {
   render() {
-    const { picture, username } = this.props;
+    const { picture, title, username } = this.props;
 
     return(
       <>
-        <div>
-          <h5>{picture.title}</h5>
+        {/* <div>
+          <h5>{title}</h5>
           <p>by {username}</p>
           <p>{picture.location}</p>
           <p>{picture.caption}</p>
-        </div>
-        <Link>
-          <img src={picture.photoUrl} alt={picture.title} />
+        </div> */}
+        <Link to={`/pictures/${picture.id}`}>
+          <img className='display-img' src={picture.photoUrl} alt={title} />
+          <p>{picture.title}</p>
         </Link>
       </>
     )
