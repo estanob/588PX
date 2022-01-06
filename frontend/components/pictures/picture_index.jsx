bec167f5fe01f1cb32dbdade80af6760d2a66a1e
Brandon <estanob@gmail.com>
2022-01-06 15:12:15 -0800

    Refactor picture index component

--- frontend/components/pictures/picture_index.jsx
+++ frontend/components/pictures/picture_index.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PictureIndexItem from './picture_index_item';
 
-class PictureIndex extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const PictureIndex = (props) => {
+  let {
+    pictures,
+    session,
+    fetchPictures,
+  } = props;
 
-  componentDidMount() {
-    this.props.fetchPictures();
-    this.props.fetchPicture(this.props.pictureId);
-  };
-
-  render() {
-    let { pictures, session } = this.props;
-    pictures = pictures ? pictures : [];
-    session = session ? session : '';
-    let pics = pictures.map((pic, i) => {
-      return (
-        <li className="pic-index-item" key={i}>
-          <PictureIndexItem 
-            picture={pic}
-            currentId={session} />
-        </li>
-      )
-    })
-    pics = pics.sort(() => Math.random() - 0.5);
-    return(
-      <div className='pic-index-container'>
-        <ul className='pic-index'>{pics}</ul>
-      </div>
-    );
-  };
+  pictures = pictures ? pictures : [];
+  session = session ? session : '';
+  let pics = pictures.map((pic, i) => {
+    return (
+      <li className="pic-index-item" key={i}>
+        <PictureIndexItem 
+          picture={pic}
+          currentId={session} />
+      </li>
+    )
+  })
+  pics = pics.sort(() => Math.random() - 0.5);
+  
+  useEffect(() => {
+    fetchPictures()
+  }, [])
+  
+  return (
+    <div className='pic-index-container'>
+      <ul className='pic-index'>{pics}</ul>
+    </div>
+  )
 };
 
 export default PictureIndex;
--- frontend/components/pictures/picture_index_container.js
+++ frontend/components/pictures/picture_index_container.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux"
 import PictureIndex from "./picture_index"
-import { fetchPicture, fetchPictures } from "../../actions/picture_actions"
-import { fetchUser } from "../../actions/profile_actions";
+import { fetchPictures } from "../../actions/picture_actions"
 
 const mSTP = state => {
   return {
@@ -12,7 +11,6 @@ const mSTP = state => {
 
 const mDTP = dispatch => {
   return {
-    fetchPicture: pictureId => dispatch(fetchPicture(pictureId)),
     fetchPictures: () => dispatch(fetchPictures()),
     fetchUser: id => dispatch(fetchUser(id)),
   };
